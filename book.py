d6e3d96ccc289a8c4d5f33eaf62b935fab15650a
Peng Xiao <xiaoquwl@gmail.com>
2023-10-27 23:59:57 +0200

    update

--- book.py
+++ book.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi import HTTPException
+from fastapi import Depends
 import uvicorn
 from sqlmodel import create_engine, SQLModel, Session, select
 
@@ -21,58 +22,65 @@ def on_startup():
     SQLModel.metadata.create_all(engine)
 
 
-@app.get("/api/books")
-def get_books(type_: str | None = None, id_: int | None = None) -> list[Book]:
+def get_session():
     with Session(engine) as session:
-        query = select(Book)
-        if type_:
-            query = query.where(Book.type_ == type_)
-        if id_:
-            query = query.where(Book.id_ == id_)
-        return session.exec(query).all()
+        yield session
+
+
+@app.get("/api/books")
+def get_books(
+    type_: str | None = None,
+    id_: int | None = None,
+    session: Session = Depends(get_session),
+) -> list[Book]:
+    query = select(Book)
+    if type_:
+        query = query.where(Book.type_ == type_)
+    if id_:
+        query = query.where(Book.id_ == id_)
+    return session.exec(query).all()
 
 
 @app.get("/api/books/{id_}")
-def get_book_by_id(id_: int) -> Book:
-    with Session(engine) as session:
-        book = session.get(Book, id_)
-        if book:
-            return book
-        else:
-            raise HTTPException(status_code=404, detail=f"No book with _id={id_}")
+def get_book_by_id(id_: int, session: Session = Depends(get_session)) -> Book:
+    book = session.get(Book, id_)
+    if book:
+        return book
+    else:
+        raise HTTPException(status_code=404, detail=f"No book with _id={id_}")
 
 
 @app.post("/api/books")
-def add_book(book: BookInput) -> Book:
-    with Session(engine) as session:
-        new_book = Book.from_orm(book)
-        session.add(new_book)
-        session.commit()
-        session.refresh(new_book)
-        return new_book
+def add_book(book: BookInput, session: Session = Depends(get_session)) -> Book:
+    new_book = Book.from_orm(book)
+    session.add(new_book)
+    session.commit()
+    session.refresh(new_book)
+    return new_book
 
 
 @app.delete("/api/books/{id_}")
-def delete_book(id_: int):
-    matches = [book for book in books if book.id_ == id_]
-    if matches:
-        books.remove(matches[0])
-        save_book(books)
+def delete_book(id_: int, session: Session = Depends(get_session)):
+    book = session.get(Book, id_)
+    if book:
+        session.delete(book)
+        session.commit()
     else:
         raise HTTPException(status_code=404, detail=f"No book with _id={id_}")
 
 
 @app.put("/api/books/{id_}")
-def update_book(id_: int, new_book: BookInput) -> BookOutput:
-    matches = [book for book in books if book.id_ == id_]
-    if matches:
-        book = matches[0]
+def update_book(
+    id_: int, new_book: BookInput, session: Session = Depends(get_session)
+) -> Book:
+    book = session.get(Book, id_)
+    if book:
         book.name = new_book.name
         book.isbn = new_book.isbn
         book.type_ = new_book.type_
         book.publish = new_book.publish
         book.price = new_book.price
-        save_book(books)
+        session.commit()
         return book
     else:
         raise HTTPException(status_code=404, detail=f"No book with _id={id_}")
